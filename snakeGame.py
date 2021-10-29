8c32de6ba9652db8fab22c6e89ba4e720b64e03f
BraunHdez <69830966+A01653004@users.noreply.github.com>
2021-10-29 08:44:27 -0500

    Food Move
    Función para que la comida se mueva.

--- snakeGame.py
+++ snakeGame.py
@@ -6,6 +6,7 @@ from freegames import square, vector
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+cont=0
 
 def change(x, y):
     "Change snake direction."
@@ -20,6 +21,7 @@ def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
+    
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
@@ -33,6 +35,7 @@ def move():
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
+    
     else:
         snake.pop(0)
 
@@ -44,6 +47,12 @@ def move():
     square(food.x, food.y, 9, colorFood)
     update()
     ontimer(move, 100)
+
+def foodMove():
+    food.x = (randrange(-3, 3)*10) + food.x
+    food.y = (randrange(-3, 3)*10) + food.y 
+    ontimer(foodMove,2000)
+    
 
 colors = ['yellow', 'orange', 'green', 'black', 'pink']
 def selected(colors):
@@ -62,4 +71,5 @@ onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
+foodMove()
 done()
